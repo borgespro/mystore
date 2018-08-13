b9f7a5bb1723336934ed9b6bb4be1ea05bd55ddd
Thiago Reis <thiagosbf@gmail.com>
2018-08-13 17:16:15 -0300

    testing static files

--- src/urls.py
+++ src/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -25,4 +24,3 @@ urlpatterns = [
     path('api/sales/', include('apps.sales.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
