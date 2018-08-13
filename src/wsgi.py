0fae40a620efad1e3d9f74bfe4e487c58dc68524
Thiago Reis <thiagosbf@gmail.com>
2018-08-13 15:57:40 -0300

    fix setup for deploy

--- src/wsgi.py
+++ src/wsgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath(
-#     os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../src")))
 
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
