096c904bd23722e8b7e8474430a5d55aa6837a4d
Thiago Reis <thiagosbf@gmail.com>
2018-08-13 16:51:21 -0300

    fix settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 INSTALLED_APPS = [
